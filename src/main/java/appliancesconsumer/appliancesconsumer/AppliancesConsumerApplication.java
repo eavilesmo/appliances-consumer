dea19584a4d7ddca2dcc453e288f223d34b9163c
Esperanza <esperanza.aviles@clarity.ai>
2023-11-20 17:23:49 +0100

    initialize project

---
+++ src/main/java/appliancesconsumer/appliancesconsumer/AppliancesConsumerApplication.java
@@ -0,0 +1,13 @@
+package appliancesconsumer.appliancesconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliancesConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliancesConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appliancesconsumer/appliancesconsumer/AppliancesConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package appliancesconsumer.appliancesconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliancesConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
